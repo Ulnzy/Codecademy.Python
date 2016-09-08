84a96a553e9fedbb3dfdba8eb8586e7a9824ca5c
Ulnzy <Happoubi@gmx.com>
2016-09-08 14:23:54 +0700

    Update reverse.py

--- reverse.py
+++ reverse.py
@@ -1,9 +1,3 @@
-#Define a function called reverse that takes a string textand returns that string in reverse.
-
-#For example: reverse("abcd") should return "dcba".
-
-    #You may not use reversed or [::-1] to help you with this.
-    #You may get a string containing special characters (for example, !, @, or #).
 
 def reverse(text):
     string = ""
